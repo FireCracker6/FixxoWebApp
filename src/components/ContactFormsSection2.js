2005c5900360ca008283599802e84649a484826a
FireCracker6 <111967393+FireCracker6@users.noreply.github.com>
2022-10-28 10:32:18 +0200

    backup

--- src/components/ContactFormsSection2.js
+++ src/components/ContactFormsSection2.js
@@ -2,17 +2,43 @@ import React from 'react'
 import { useState } from 'react'
 
 function ContactFormsSection2() {
- 
-    const [contactForm, setContactForm] = useState ({name: ``, email: ``, comments: ``})
+    const initFormValues = ({name: ``, email: ``, comments: ``})
+    const [contactForm, setContactForm] = useState ({initFormValues})
 
     const [formErrors, setFormErrors] = useState({})
     const [canSubmit, setCanSubmit] = useState(false)
-    const [checkField, setCheckField] = useState(false)
+   
 
  
 
-    const validate = (values) => {
-        const errors = {}
+    const validate = (e, contactForm = null) => {
+
+        if (e.type === 'submit') {
+            const errors = {}
+
+            errors.name = validate_name(contactForm.name)
+            errors.email = validate_email(contactForm.email)
+            errors.comment = validate_comment(contactForm.comments)
+            return errors
+        }
+
+        else {
+        const {id, value} = e.target
+
+        switch(id) {
+            case 'name':
+                return validate_name(value)
+ 
+            case 'email':
+                return validate_email(value)
+ 
+            case 'comment':
+                return validate_comment(value)
+    
+        }
+
+    }
+      /*   const errors = {}
         const regex_email =  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
 
         if (!values.name) {
@@ -38,29 +64,64 @@ function ContactFormsSection2() {
         setCanSubmit(true)
         else 
         setCanSubmit(false)
+ */
+     
+     
+      /*   return errors; */
 
-     
-     
-        return errors;
+      return validate
     }
 
     const handleChange = (e) => {
         const {id, value} = e.target 
-        setContactForm({...contactForm, [id]: value })
+        setContactForm({...ContactFormsSection2, [id]: value })
+        setFormErrors({...formErrors, [id]: validate(e) })
     }
 
 
-
-
+ 
        
         const handleSubmit = (e) => {
 
             e.preventDefault();
-            setFormErrors(validate(contactForm))
+            setFormErrors(validate(e, contactForm))
+
+            if (formErrors.name === null && formErrors.email === null && formErrors.comment === null) {
+                setCanSubmit(true)
+                setContactForm(initFormValues)
+            }
+           setContactForm(initFormValues)
 
         }
 
+        const handleKeyUp = (e) => {
 
+            const {id, value} = e.target 
+            setFormErrors({...formErrors, [id]: value })
+            console.log('key up triggered')
+        }
+
+        const validate_name = (value) => {
+            if (!value)
+                return 'A name is required'
+            else if (value.length > 2)
+                return 'Must be a valid name'
+            else 
+            return null
+        }
+        const validate_email = (value) => {
+            const regex_email =  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+
+            if (!value)
+                return 'An email is required'
+            else if (!regex_email.text(value))
+            return 'Must be a valid email address'
+            else return null
+        }
+        const validate_comment = (value) => {
+            if (!value)
+                return 'A comment is required'
+        }
             
       
        
@@ -77,21 +138,22 @@ function ContactFormsSection2() {
            <div className="container">
          
               {
-                canSubmit ? <div className='d-flex justify-content-center align-items-center' style={{height: '400px'}} ><div className='header'><h2>Thank you for your comment! {contactForm.name}</h2></div></div>
-                : <>
+                canSubmit ? (<div className='d-flex justify-content-center align-items-center' style={{height: '400px'}} ><div className='header'><h2>Thank you for your comment! {contactForm.name}</h2></div></div>)
+                : 
+                (<>
                  <form onSubmit={handleSubmit} noValidate>
-               <div className="header">
-               <h2>Come in Contact with Us </h2>
+                    <div className="header">
+                    <h2>Come in Contact with Us </h2>
               
-           </div>
+                    </div>
                 <div className="item-1">
-                    <label htmlFor="name" id='name-label' className='errorMessage' value={contactForm.name}>{formErrors.name}</label>
-                    <input  id='name'  type='text' value={contactForm.name}  onChange={handleChange} placeholder='Your Name' required/>
-                    <div className='errorMessage'> {formErrors.name} </div>
+                   
+                    <input  id="name"  type="text" value={contactForm.name}  onChange={handleChange} placeholder="Your Name"  required />
+                    <div className="errorMessage"> {formErrors.name} </div>
                 </div>
                   
                 <div className="item-2">
-                <label htmlFor="email" id='email-label'></label>
+               
                     <input id='email' type='email'  value={contactForm.email}  onChange={handleChange}  placeholder='Your Mail' />
                     <div className='errorMessage'> {formErrors.email} 
                     </div>
@@ -107,7 +169,7 @@ function ContactFormsSection2() {
                 </div>
              
        </form>
-                </>
+                </>)
               }
            </div>
        </div>
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
 
-import BreadCrumbsSections from './sections/BreadCrumbsSections'
+
 import ContactFormsSection2 from './ContactFormsSection2'
-import { useState } from 'react'
-import { MapSection } from './sections/MapSection'
+
 
 function Contacts() {
   return (
