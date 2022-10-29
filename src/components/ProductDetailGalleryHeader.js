a8f0bdb286f401b2ae976aec588bbd0812219e2b
FireCracker6 <111967393+FireCracker6@users.noreply.github.com>
2022-10-29 19:33:26 +0200

    backup

---
+++ src/components/ProductDetailGalleryHeader.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import MobileGridCard from './MobileGridCard'
+import ProductCard2 from './ProductCard2'
+
+
+export const ProductDetailGalleryHeader = ({title, products}) => {
+
+
+    const [width, setWidth] = React.useState(window.innerWidth);
+  const breakpoint = 1200;
+
+  React.useEffect(() =>  {
+
+    
+    window.addEventListener("resize", () => setWidth(window.innerWidth));
+  }, [] );
+
+  
+  return width < breakpoint ? 
+    <>
+    <div class="products-gallery-header">
+    <div class="container">
+    <div class="header">
+    <div class="item-1">
+        <h2>{title}</h2>
+    </div>
+    <div class="item-2">
+    <button class="circle"><i class="fa-solid fa-chevron-right fa-flip-horizontal"></i></button>
+    <button class="circle"><i class="fa-solid fa-chevron-right "></i></button>
+    </div>
+    </div>
+  
+        </div>
+        
+        <div className="container-md ">
+
+      
+     <div className="row row-cols-1 row-cols-sm-2  g-4  row-cols-md-2 g-4   row-cols-xl-4  g-4">
+
+        
+     {
+
+products.map(product =>     <MobileGridCard key={product.id} item={product} />) 
+
+
+}
+        </div>
+    </div>
+    </div>
+    </> : 
+
+<>
+<div class="products-gallery-header">
+<div class="container">
+<div class="header">
+<div class="item-1">
+    <h2>{title}</h2>
+</div>
+<div class="item-2">
+    <button class="circle"><i class="fa-solid fa-chevron-right fa-flip-horizontal"></i></button>
+    <button class="circle"><i class="fa-solid fa-chevron-right "></i></button>
+</div>
+</div>
+
+    </div>
+    
+    <div className="container ">
+
+  
+    <div className="product-gallery-grid"> 
+
+    
+ {
+
+products.map(product =>     <ProductCard2 key={product.id} item={product} />) 
+
+
+}
+    </div>
+</div>
+</div>
+</> ;
+
+    
+  
+}
--- src/views/ProductDetailsView.js
+++ src/views/ProductDetailsView.js
@@ -7,11 +7,15 @@ import BreadCrumbsSections from '../components/sections/BreadCrumbsSections'
 import { ProductDetailsInfoGrid } from '../components/ProductDetailsInfoGrid'
 import { QuantityButton } from '../components/QuantityButton'
 import { useState } from 'react'
+import { ProductDetailGalleryHeader } from '../components/ProductDetailGalleryHeader'
 
 function ProductDetailsView() {
   const [featuredProducts, setFeaturedProducts ] = useState([
-    {id: 1, productName: "Penguin Blouse", category: "Fashion", price: "$20.50", rating: 5, img: "https://images.pexels.com/photos/1299391/pexels-photo-1299391.jpeg?cs=srgb&dl=pexels-david-dibert-1299391.jpg&fm=jpg"}
-  
+    {id: 1, productName: "Penguin Blouse", category: "Fashion", price: "$20.50", rating: 5, img: "https://images.pexels.com/photos/1299391/pexels-photo-1299391.jpeg?cs=srgb&dl=pexels-david-dibert-1299391.jpg&fm=jpg"},
+    {id: 2, productName: " BW Penquin", category: "Fashion", price: "$35.00", rating: 5, img: "https://images.pexels.com/photos/9085057/pexels-photo-9085057.jpeg?cs=srgb&dl=pexels-taryn-elliott-9085057.jpg&fm=jpg"},
+    {id: 3, productName: "Modern Black PW", category: "Fashion", price: "$55.90", rating: 5, img: "https://images.pexels.com/photos/7177267/pexels-photo-7177267.jpeg?cs=srgb&dl=pexels-jeffrey-eisen-7177267.jpg&fm=jpg"},
+    {id: 4, productName: "Modern Black Blouse", category: "Fashion", price: "$42.90", rating: 5, img: "https://images.pexels.com/photos/9393990/pexels-photo-9393990.jpeg?cs=srgb&dl=pexels-chris-f-9393990.jpg&fm=jpg"}
+
   ])
   
   return (
@@ -26,6 +30,8 @@ function ProductDetailsView() {
     <TopBannerDiscount />
     <BreadCrumbsSections currentPage="Product Details" /> 
     <ProductDetailsInfoGrid products={featuredProducts} />
+    <ProductDetailGalleryHeader title="Related Products" products={featuredProducts}  />
+    <div className="container" style={{height: "100px"}}></div>
     <Footer />   
     </div>
   )
